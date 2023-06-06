59d4dc58333e669a3a683ea572b7d9bed3ac98f7
Anton <B_atonyo_S@bk.ru>
2023-06-06 20:50:34 +0300

    Task40

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,7 @@
 Семинар 6. 06.06.2023
+double [] InAr (string array)
+{
+    string [] Nums = array.Split(" ");
+    double [] NumDoub = new double[Nums.Length];
+    for (int i = 0; i<Nums.Length; i++) NumDoub[i]=double.Parse(Nums[i]);
+    return NumDoub;
---
+++ Task40/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*// **Задача 40:**Напишите программу, 
+// которая принимает на вход три числа и проверяет, может ли 
+// существовать треугольник с сторонами такой длины.
+
+
+// ❗ **Теорема о неравенстве треугольника:
+// **каждая сторона треугольника меньше суммы двух других сторон.
+*/
+bool Treangle (int a, int b, int c)
+{
+ return a+b>c&&b+c>a&&a+c>b;
+ }
+Console.Clear();
+Console.WriteLine("Введите длины сторон");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int с = int.Parse(Console.ReadLine());
+if (Treangle (a,b,с)) System.Console.WriteLine("Да");
+else System.Console.WriteLine("Нет"); 

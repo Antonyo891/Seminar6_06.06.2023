359288920a5bd60fdbb584eaa918fcdba5a42686
Anton <B_atonyo_S@bk.ru>
2023-06-09 08:37:06 +0300

    Task42 better

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -8,30 +8,14 @@
 
 // 2 -> 10
 
-int[] DecToBin(int n)
-{ int [] result = {1};
-    string backword = "";
-    if (n==1) return result;
-     else {while (n >= 1)
-    {
-        //System.Console.WriteLine($"n%2 = {n%2}");
-       //System.Console.WriteLine($"backword = {backword}");
-        //System.Console.WriteLine($"n=  {n}");
-        if (n==1) backword = backword + n % 2;
-        else backword = backword + n % 2 + " ";
-        n = n / 2;
-            }}
-    string[] resultString = backword.Split(" ");
-    result = new int[resultString.Length];
-    int temporary = 0;
-    for (int i = 0; i < result.Length / 2; i++)
-    {
-        temporary = int.Parse(resultString[i]);
-        result[i] = int.Parse(resultString[resultString.Length - 1 - i]);
-        result[resultString.Length - 1 - i] = temporary;
-    }
-    System.Console.WriteLine(backword);
-    return result;
+string DecToBin(int n)
+{     string backword = "";
+    //if (n==1) return result;
+    // else {
+        while (n > 0) {
+                   backword = n % 2 + backword;
+        n = n/2;}
+        return backword;
 }
 Console.WriteLine("Введите число");
 int Number = int.Parse(Console.ReadLine());
